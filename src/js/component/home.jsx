bfa3c6299bac17235fc091ad5931542ffa58282d
Fuenzalidaagu <fuenzalidaagustin13@gmail.com>
2022-12-22 03:26:23 +0000

    Todolist with React and Fetch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,85 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function TodoList() {
+  const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState('');
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  function handleTaskInput(event) {
+    setTaskInput(event.target.value);
+  }
 
-export default Home;
+  async function addTask(event) {
+    if (event.key === 'Enter') {
+      const trimmedTaskInput = taskInput.trim();
+  
+      if (!trimmedTaskInput) {
+        return;
+      }
+  
+      const response = await fetch('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', {
+        method: 'PUT',
+        body: JSON.stringify({ task: trimmedTaskInput }),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+  
+      setTasks([...tasks, trimmedTaskInput]);
+      setTaskInput('');
+    }
+  }
+  
+  async function deleteTask(index) {
+
+    await fetch('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', {
+      method: 'PUT',
+      body: JSON.stringify({ tasks: tasks.filter((task, i) => i !== index) }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    setTasks(tasks.filter((task, i) => i !== index));
+  }
+
+  async function cleanAllTasks() {
+
+    await fetch('https://assets.breatheco.de/apis/fake/todos/user/alesanchezr', {
+      method: 'DELETE'
+    });
+  
+    setTasks([]);
+  }
+
+  return (
+    <div className='book'>
+      <div className="todo-list">
+        <h1>TODO LIST</h1>
+        <input
+          type="text"
+          placeholder="Añadir tarea"
+          value={taskInput}
+          onChange={handleTaskInput}
+          onKeyPress={addTask}
+        />
+        {tasks.length > 0 ? (
+          <ul>
+            {tasks.map((task, index) => (
+              <li key={index}>
+                {task}
+                <button onClick={() => deleteTask(index)}>x</button>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No hay tareas, añadir tareas</p>
+        )}
+        <div  className='deleteall'>
+        <button onClick={cleanAllTasks}>Borrar todas las tareas</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoList;
